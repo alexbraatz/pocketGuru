8ccc2826ac72e7928c594262d132c264dfb3e77f
alex braatz <alexjbraatz@gmail.com>
2021-04-23 10:32:36 -0500

    create gurus user db

--- database.sql
+++ database.sql
@@ -3,8 +3,8 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+CREATE TABLE gurus (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR (80) UNIQUE NOT NULL,
+    secret_code VARCHAR (1000) NOT NULL
 );
