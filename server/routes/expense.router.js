1bc525d696a026b5880294d8936386dfd5956e82
alex braatz <alexjbraatz@gmail.com>
2021-04-26 19:34:31 -0500

    POST route gettin closlier

--- server/routes/expense.router.js
+++ server/routes/expense.router.js
@@ -12,12 +12,19 @@ router.get('/', (req, res) =>{
 
 });
 
-router.post('/expense', (req, res) => {
+router.post('/', (req, res) => {
     console.log( 'in expense.router POST req.body:', req.body )
     const sqlCmd = `
     INSERT INTO expenses (guru_id, amount, description, expense_category)
-    VALUES ($1, $2, $3, $4) RETURNING id
+    VALUES ($1, $2, $3, $4)
     `
+    pool.query( sqlCmd, [ req.body.guru_id, req.body.amount, req.body.description, req.body.expense_category ])
+    .then( result => {
+        res.sendStatus(201);
+    }).catch( error => {
+        console.log ( 'in expense.router POST error:', error );
+        res.sendStatus(500);
+    })
 });
 
 module.exports = router
